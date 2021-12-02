521a961eb5c466af23e0fbcaca2f0d4be759fe68
Rushit <90950237+rushit5560@users.noreply.github.com>
2021-12-02 10:47:09 +0530

    Category Api

--- cosmetic_store/lib/screens/category_screen/category_screen.dart
+++ cosmetic_store/lib/screens/category_screen/category_screen.dart
@@ -1,76 +1,73 @@
-import 'package:cosmetic_store/common/app_images.dart';
+import 'package:cosmetic_store/common/api_url.dart';
 import 'package:cosmetic_store/common/app_color.dart';
+import 'package:cosmetic_store/common/common_widgets.dart';
+import 'package:cosmetic_store/controllers/category_screen_controller/category_screen_controller.dart';
 import 'package:cosmetic_store/screens/cart_screen/cart_screen.dart';
 import 'package:cosmetic_store/screens/collection_screen/collection_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class CategoryScreen extends StatelessWidget {
-
-  final categoryList = [
-    AppImages.category1, AppImages.category2,
-    AppImages.category3, AppImages.category4,
-    AppImages.category5, AppImages.category6,
-    AppImages.category7, AppImages.category8,
-    AppImages.category9, AppImages.category10,
-    AppImages.category11, AppImages.category12,
-    AppImages.category11, AppImages.category12,
-    AppImages.category11, AppImages.category12,
-  ];
+  CategoryScreenController categoryScreenController = Get.put(CategoryScreenController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Categories',
-        style: TextStyle(color: Colors.black),
+        title: Text(
+          'Categories',
+          style: TextStyle(color: Colors.black),
         ),
         actions: [
           IconButton(
-              onPressed: (){
-                Get.to(()=> CartScreen());
-              },
-              icon: Icon(Icons.shopping_cart_rounded),
+            onPressed: () {
+              Get.to(() => CartScreen());
+            },
+            icon: Icon(Icons.shopping_cart_rounded),
           ),
         ],
       ),
-
-      body: GridView.builder(
-        itemCount: categoryList.length,
-        physics: BouncingScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-        ),
-        itemBuilder: (context ,index) {
-          return Padding(
-            padding: const EdgeInsets.all(5),
-            child: GestureDetector(
-              onTap: () {
-                print('Clicked On Index Number : $index');
-                Get.to(()=> CollectionScreen());
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppColors.kTometoColor,
+      body: Obx(
+        () => categoryScreenController.isLoading.value
+            ? CustomCircularProgressIndicator()
+            : GridView.builder(
+                itemCount: categoryScreenController.categoryLists.length,
+                physics: BouncingScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: Container(
-                    decoration: BoxDecoration(
-                          color: Colors.white,
-                        shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage(categoryList[index]),
-                      )
+                itemBuilder: (context, index) {
+                  final imgUrl = ApiUrl.ApiMainPath +
+                      '${categoryScreenController.categoryLists[index].showimg}';
+                  return Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: GestureDetector(
+                      onTap: () {
+                        print('Clicked On Index Number : $index');
+                        Get.to(() => CollectionScreen());
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.kTometoColor,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: NetworkImage('$imgUrl'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                },
               ),
-            ),
-          );
-        },
       ),
     );
   }
