e807048ee66995c5624e818eec98edb4ca885466
Rushit <90950237+rushit5560@users.noreply.github.com>
2021-12-03 18:04:14 +0530

    Change Password Screen Reformat

---
+++ cosmetic_store/lib/screens/change_password_screen/change_password_screen_widgets.dart
@@ -0,0 +1,128 @@
+import 'package:cosmetic_store/common/app_color.dart';
+import 'package:cosmetic_store/controllers/change_password_screen_controller/change_password_screen_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ChangePasswordFieldsModule extends StatelessWidget {
+  // const ChangePasswordFieldsModule({Key? key}) : super(key: key);
+  final changePasswordScreenController = Get.find<ChangePasswordScreenController>();
+  GlobalKey<FormState> formKey;
+  TextEditingController oldPasswordFieldController;
+  TextEditingController newPasswordFieldController;
+  TextEditingController cNewPasswordFieldController;
+
+
+  ChangePasswordFieldsModule({
+    required this.formKey,
+    required this.oldPasswordFieldController,
+    required this.newPasswordFieldController,
+    required this.cNewPasswordFieldController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: oldPasswordFieldController,
+            keyboardType: TextInputType.text,
+            decoration: inputDecoration('Old Password'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Old Password Field not Empty";
+              }
+            },
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: newPasswordFieldController,
+            keyboardType: TextInputType.text,
+            decoration: inputDecoration('New Password'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "New Password Field not Empty";
+              }
+            },
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: cNewPasswordFieldController,
+            keyboardType: TextInputType.text,
+            decoration: inputDecoration('Confirm New Password'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Confirm New Password Field not Empty";
+              } else if (newPasswordFieldController.text !=
+                  cNewPasswordFieldController.text) {
+                return "Password Not Match";
+              }
+            },
+          ),
+          const SizedBox(height: 25),
+          GestureDetector(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                changePasswordScreenController.getForgotPasswordData(
+                    "${oldPasswordFieldController.text.trim()}",
+                    "${newPasswordFieldController.text.trim()}",
+                    "${cNewPasswordFieldController.text.trim()}");
+                oldPasswordFieldController.clear();
+                newPasswordFieldController.clear();
+                cNewPasswordFieldController.clear();
+              }
+            },
+            child: Container(
+              width: Get.width,
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 10),
+                child: Text(
+                  'Submit',
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: AppColors.kTometoColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(hintText) {
+    return InputDecoration(
+      hintText: '$hintText',
+      isDense: true,
+      contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 14),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(35),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(35),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(35),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(35),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+    );
+  }
+
+
+}
